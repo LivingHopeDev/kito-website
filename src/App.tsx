b0d512b9efc97fedc0cb0bd3806a7bf25913477c
adetayo adewobi <adewobiadetayo1@gmail.com>
2024-09-09 15:14:13 +0100

    setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold underline text-blue-500">
+        Hello, Vite + React + Tailwind!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
